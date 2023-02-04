2616e57d019280e7f1c8137628a1beafc348435a
Nagateja <teja91.valiveti@gmail.com>
2023-02-04 22:06:29 +0530

    Pages Created

---
+++ clients/src/Pages/index.js
@@ -0,0 +1,6 @@
+export {default as Register} from "./Register";
+export {default as Login} from "./Login";
+export {default as Profile} from "./Profile";
+export {default as PageNotFound} from "./PageNotFound";
+export {default as PasswordReset} from "./PasswordReset";
+export {default as PasswordRecovery} from "./PasswordRecovery";
